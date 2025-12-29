b5c47eaef0988ff16fef969264dd08adafdb2b6f
yourweb3bro <idrisabdulrazak230@gmail.com>
2025-12-29 12:58:05 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
-  base: '/coin-verge/'
+  base: '/coin-verge/',
 })
